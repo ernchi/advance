5bd0c1b563b2437f1f5b83d09edce1b45200e92f
Alex <7Alex7Li@gmail.com>
2018-11-05 18:31:42 -0500

    Add survey.js

--- survey/genQuestion.js
+++ survey/genQuestion.js
@@ -1,8 +1,8 @@
 function genQuestion(question,num){
-	process.stdout.write("<div class=\"btn-group\">\r\n");
+	process.stdout.write("<div class=\"response-btn\">\r\n");
 	process.stdout.write("  <div class=\"questions\">\r\n");
 	process.stdout.write("    <div class=\"col-1\"></div>\r\n");
-	process.stdout.write("    <div class=\"col-6\">Q" + num + " " + question + "</div>\r\n");
+	process.stdout.write("    <div class=\"col-6\">Q" + num + ". " + question + "</div>\r\n");
 	for (var i = 1; i <= 4; i++) {
 			var name = "btn" + i + num;
     		process.stdout.write("    <div class=\"col-1\"><input type=\"radio\" id=\"" + name);
@@ -17,6 +17,6 @@ questions = ["What's your favorite color",
 			"On a scale of 1 to 4, rate how depressed you are. If you are very depressed and want to kill yourself, pick 1. If your level of depression is higher than that, then pick a higher number.",
 			"This question is great.",
 			"How are you?"];
-for(var i=1; i<=questions.length; i++){
+for(var i = 1; i <= questions.length; i++){
 	genQuestion(questions[i-1],i);
 }
