7ac318e6ab5c9158e1c03f24eeb14fd19eb4685a
ernchi <khoo.ernchi@gmail.com>
2018-11-05 19:13:36 -0500

    Added options bar in survey page

--- survey/survey.js
+++ survey/survey.js
@@ -1,9 +1,9 @@
 //https://www.w3schools.com/jquery/jquery_ajax_get_post.asp
 $( document ).ready(function() {
 	$( "input" ).click(function( event ) {
-		alert( "You have selected a response." );
+
 	});
 	$.get("https://codeforces.com/api/contest.ratingChanges?contestId=1074", function(data, status){
-        alert("Data: " + data + "\nStatus: " + status);
+      
     });
 });
