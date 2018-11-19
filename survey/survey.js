effe54cb1ae5009801f2756f3382b12b24be8ea5
ernchi <khoo.ernchi@gmail.com>
2018-11-19 18:41:46 -0500

    Fixed options bar

--- survey/survey.js
+++ survey/survey.js
@@ -1,3 +1,4 @@
+// Generates the questions
 function genQuestionHtml(question, num){
 	var str = "<div class=\"response-btn\">\r\n"
 	str = str.concat("  <div class=\"questions\">\r\n");
@@ -15,18 +16,39 @@ function genQuestionHtml(question, num){
 	return str;
 }
 
+// Generates options bar
+function genOptions(options){
+	var str = "    <div class=\"col-1\"><p>";
+	str = str.concat(options);
+	str = str.concat("</p></div>");
+	return str;
+}
+
+$( document ).ready(function() {
+	$.get("https://codeforces.com/api/contest.ratingChanges?contestId=1074", function(data, status){
+
+    });
+
+	options = ["Not at all", "Several days", "More than half the days", "Nearly every day"];
+	$(".options").append("<div class=\"col-7\"></div>");
+	for(var i = 0; i < options.length; i++){
+		$(".options").append(genOptions(options[i]));
+	}
+	$(".options").append("<div class=\"col-1\"></div>");
+
+	$.get("https://codeforces.com/api/contest.ratingChanges?contestId=1074", function(data, status){});
+});
 
 //https://www.w3schools.com/jquery/jquery_ajax_get_post.asp
 $( document ).ready(function() {
 	$.get("https://codeforces.com/api/contest.ratingChanges?contestId=1074", function(data, status){
-      
+
     });
 
 	questions = ["What's your favorite color",
 	"On a scale of 1 to 4, rate how depressed you are. If you are very depressed and want to kill yourself, pick 1. If your level of depression is higher than that, then pick a higher number.",
 	"This question is cool.",
 	"How are you?"];
-	$(".questions").append("HIIIIIIIIIIIIII");
 	for(var i = 1; i <= questions.length; i++){
 		$(".questions").append(genQuestionHtml(questions[i-1],i));
 	}
