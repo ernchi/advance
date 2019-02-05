90170b8df68d131340d787ef18be46dc9c0c8760
ernchi <khoo.ernchi@gmail.com>
2019-02-04 20:30:10 -0500

    Articles page update

---
+++ Articles/well-being.js
@@ -0,0 +1,48 @@
+jsonobj = {
+	"article": [
+	{
+		{
+			"id": 1,
+			"header": "99 Coping Skills",
+			"description": "Provides an interactive list and print-out of 99 coping skills and strategies.",
+      "img":,
+      "link": "http://www.yourlifeyourvoice.org/pages/tip-99-coping-skills.aspx"
+		},
+		{
+			"id": 2,
+			"header": "Calm",
+			"description": "This website provides some meditation techniques that are helpful for calmness, anxiety, sleep, etc",
+      "img":,
+      "link": "https://www.calm.com/"
+		},
+    {
+			"id": 3,
+			"header": "DailyGood.org",
+			"description": "Perhaps you’re stressed out by all the depressing news you’re hearing? Get a dose of happiness at Daily Good, which only posts uplifting, inspiring news.",
+      "img":,
+      "link": "http://www.dailygood.org/"
+		},
+    {
+			"id": 4,
+			"header": "Allolo",
+			"description": "Website that offers various sounds to help calm and relax anyone who needs it.",
+      "img":,
+      "link": "http://www.allolo.ru/?id=5"
+		},
+    {
+			"id": 5,
+			"header": "National Health Services Directory",
+			"description": "You can go to a doctor for any physical or mental health issues. A doctor (GP) can diagnose health problems, as well as recognise symptoms of mental health difficulties you might be experiencing. They can refer you to see a specialist if you need one.",
+      "img":,
+      "link": "https://about.healthdirect.gov.au/nhsd"
+		},
+    {
+			"id": 6,
+			"header": "Grounding: Create Personal Calm",
+			"description": "Learn about and put into practice the use of grounding techniques during a heated moment.",
+      "img":,
+      "link": "https://www.winona.edu/resilience/Media/Grounding-Worksheet.pdf"
+		}
+	}
+  ]
+}
