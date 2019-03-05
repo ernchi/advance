a3412b125c5bb117063ce0352f789436f8ea1197
ernchi <khoo.ernchi@gmail.com>
2019-03-04 20:40:14 -0500

    Added pictures to well-being articles page

--- Articles/well-being.js
+++ Articles/well-being.js
@@ -14,6 +14,16 @@ function genDescription(description) {
   return str;
 }
 
+/*Generates the images for the articles*/
+function genImage(img) {
+  var str = "<div class=\"card-h-100\">";
+  str = str.concat("<a href=\"#\">");
+  str = str.concat("<img class=\"card-img-top\" src=\"");
+  str = str.concat(img);
+  str = str.concat("\"></a>");
+  return str;
+}
+
 $(document).ready(function() {
   var numberOfArticles = 0; // number of articles
   articles = jsonobj["article"];
@@ -53,6 +63,7 @@ $(document).ready(function() {
     // keep track of which numbers have been used
     numArray.push(randomNum);
 
+    $(".card-" + i).append(genImage(articleImg[randomNum]));
     $(".card-title-" + i).append(genHeader(articleHeader[randomNum], articleLink[randomNum]));
     $(".card-text-" + i).append(articleDescription[randomNum]);
   }
@@ -64,42 +75,42 @@ jsonobj = {
 			"id": 1,
 			"header": "99 Coping Skills",
 			"description": "Provides an interactive list and print-out of 99 coping skills and strategies.",
-      "img": "",
+      "img": "../Articles/img/99copingskills.jpg",
       "link": "http://www.yourlifeyourvoice.org/pages/tip-99-coping-skills.aspx"
 		},
 		{
 			"id": 2,
 			"header": "Calm",
 			"description": "This website provides some meditation techniques that are helpful for calmness, anxiety, sleep, etc",
-      "img": "",
+      "img": "../Articles/img/calm.jpg",
       "link": "https://www.calm.com/"
 		},
     {
 			"id": 3,
 			"header": "DailyGood.org",
 			"description": "Perhaps you’re stressed out by all the depressing news you’re hearing? Get a dose of happiness at Daily Good, which only posts uplifting, inspiring news.",
-      "img": "",
+      "img": "../Articles/img/dailygood.jpg",
       "link": "http://www.dailygood.org/"
 		},
     {
 			"id": 4,
 			"header": "Allolo",
 			"description": "Website that offers various sounds to help calm and relax anyone who needs it.",
-      "img": "",
+      "img": "../Articles/img/allolo.jpg",
       "link": "http://www.allolo.ru/?id=5"
 		},
     {
 			"id": 5,
 			"header": "National Health Services Directory",
 			"description": "You can go to a doctor for any physical or mental health issues. A doctor (GP) can diagnose health problems, as well as recognise symptoms of mental health difficulties you might be experiencing. They can refer you to see a specialist if you need one.",
-      "img": "",
+      "img": "../Articles/img/nhs.jpg",
       "link": "https://about.healthdirect.gov.au/nhsd"
 		},
     {
 			"id": 6,
 			"header": "Grounding: Create Personal Calm",
 			"description": "Learn about and put into practice the use of grounding techniques during a heated moment.",
-      "img": "",
+      "img": "../Articles/img/grounding.jpg",
       "link": "https://www.winona.edu/resilience/Media/Grounding-Worksheet.pdf"
 		}
   ]
